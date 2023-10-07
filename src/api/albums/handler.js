b3733b4b80203dd97fb2b5790cabc40f36c7ad42
yat2yat <ahmadhdyt12@gmai.com>
2023-10-07 10:03:50 +0700

    init commit

---
+++ src/api/albums/handler.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-underscore-dangle */
+const autoBind = require('auto-bind');
+const SongsService = require('../../services/postgres/songsService');
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._service = service;
+    this._validator = validator;
+    this.songService = new SongsService();
+
+    autoBind(this);
+  }
+
+  async postAlbumHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+    const albumId = await this._service.addAlbum(request.payload);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Album berhasil ditambahkan',
+      data: {
+        albumId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async getAlbumByIdHandler(request) {
+    const { id } = request.params;
+    const album = await this._service.getAlbumById(id);
+    const songs = await this.songService.getSongsByAlbumId(id);
+
+    // Gabungkan data album dan data lagu
+    album.songs = songs;
+
+    return {
+      status: 'success',
+      data: {
+        album,
+      },
+    };
+  }
+
+  async putAlbumByIdHandler(request) {
+    this._validator.validateAlbumPayload(request.payload);
+    const { id } = request.params;
+
+    await this._service.editAlbumById(id, request.payload);
+
+    return {
+      status: 'success',
+      message: 'Album berhasil diperbarui',
+    };
+  }
+
+  async deleteAlbumByIdHandler(request) {
+    const { id } = request.params;
+    await this._service.deleteAlbumById(id);
+    return {
+      status: 'success',
+      message: 'Album berhasil dihapus',
+    };
+  }
+}
+
+module.exports = AlbumsHandler;
---
+++ src/server.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const AlbumsService = require('./services/postgres/albumsService');
+const SongsService = require('./services/postgres/songsService');
+const AlbumsValidator = require('./validator/albums');
+const SongsValidator = require('./validator/songs');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+const ClientError = require('./exceptions/clientError');
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+
+  const server = Hapi.server({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    },
+  ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof Error) {
+      // penanganan client error internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue;
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+        // error: response.message, // menampilkan pesan error
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue;
+  });
+
+  await server.start();
+  // eslint-disable-next-line no-console
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
